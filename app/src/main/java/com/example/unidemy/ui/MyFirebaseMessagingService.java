f23b570535a7f1220805c8dd47a936f10e97cc2e
pablomariaarranzpou <1o233Hm3>
2022-06-04 12:43:59 +0200

    Opción de ver en línea o descargar

--- app/src/main/java/com/example/unidemy/ui/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/unidemy/ui/MyFirebaseMessagingService.java
@@ -25,7 +25,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
         String channelId = "Default";
         NotificationCompat.Builder builder = new  NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.unidemy_logo_icon_v2)
+                .setSmallIcon(R.drawable.unidemy_logo_icon_final)
                 .setContentTitle(remoteMessage.getNotification().getTitle())
                 .setContentText(remoteMessage.getNotification().getBody()).setAutoCancel(true).setContentIntent(pendingIntent);;
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
--- app/src/main/java/com/example/unidemy/ui/ViewDocument.java
+++ app/src/main/java/com/example/unidemy/ui/ViewDocument.java
@@ -1,23 +1,37 @@
 package com.example.unidemy.ui;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.unidemy.R;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class ViewDocument extends AppCompatActivity {
 
     WebView webView;
     DocumentCard dc;
+    int option_d = 0, option_a = 0;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_document);
-
+        option_a = 0;
+        option_d = 1;
         webView = (WebView) findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setUseWideViewPort(true);
@@ -26,13 +40,75 @@ public class ViewDocument extends AppCompatActivity {
         String pdf;
 
 
-        if (getIntent().hasExtra("selectedDocument")) {
-            dc = (DocumentCard) getIntent().getParcelableExtra("selectedDocument");
-            pdf = dc.getDocument_url();
-            webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
-        }else{
-            pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
-            webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
-        }
+
+        downloadOrOnlineDialog();
+
     }
+
+
+
+    public void downloadOrOnlineDialog(){
+
+            final Dialog dialogDownloadOrOnline = new Dialog(ViewDocument.this);
+        dialogDownloadOrOnline.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            if (dialogDownloadOrOnline.getWindow() != null) {
+                ColorDrawable colorDrawable = new ColorDrawable(Color.TRANSPARENT);
+                dialogDownloadOrOnline.getWindow().setBackgroundDrawable(colorDrawable);
+            }
+        dialogDownloadOrOnline.setContentView(R.layout.dialog_document_view);
+        dialogDownloadOrOnline.setCancelable(false);
+        dialogDownloadOrOnline.show();
+
+            TextView resumeText = (TextView) dialogDownloadOrOnline.findViewById(R.id.correct_answer);
+
+
+            Button btn_descargar = (Button) dialogDownloadOrOnline.findViewById(R.id.dialogDescargar);
+            Button btn_enlinea = (Button) dialogDownloadOrOnline.findViewById(R.id.dialogEnLinea);
+
+
+            btn_descargar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialogDownloadOrOnline.dismiss();
+                    String pdf;
+                    if (getIntent().hasExtra("selectedDocument")) {
+
+                        dc = (DocumentCard) getIntent().getParcelableExtra("selectedDocument");
+                        downloadOrOnlineDialog();
+                        pdf = dc.getDocument_url();
+                        Uri webpage = Uri.parse(pdf);
+                        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                        startActivity(intent);
+
+                    }else{
+                        pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+                        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+                    }
+                    finish();
+                }
+            });
+
+
+
+        btn_enlinea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogDownloadOrOnline.dismiss();
+                String pdf;
+                if (getIntent().hasExtra("selectedDocument")) {
+                    dc = (DocumentCard) getIntent().getParcelableExtra("selectedDocument");
+                    pdf = dc.getDocument_url();
+                    webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+                }else{
+                    pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+                    webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+                }
+            }
+        });
+
+
+    }
+
+
+
 }
